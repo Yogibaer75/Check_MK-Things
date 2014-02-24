717c1f29bf5985494c8ca373a8796eabc01c7393
Andreas Doehler <andreas.doehler@gmail.com>
2014-02-24 14:07:29 +0100

    Dashboard nur mit Services und Hosts welche auch Benachrichtigung aktiviert haben

---
+++ dashboard_mit_benachrichtigung_only/web_plugins_dashboard/dashboard_sws.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2013             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# ails.  You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+builtin_dashboards["main_notify"] = {
+    "title" : _("Uebersicht SWS mit Benachrichtigung"),
+    "dashlets" : [
+        {
+            "title"      : _("Host Statistics"),
+            "url"        : "dashlet_hoststats.py",
+            "position"   : (1, 1),
+            "size"       : (30, 18),
+            "shadow"     : True,
+            "background" : True,
+            "refresh"    : 60,
+        },
+        {
+            "title"      : _("Service Statistics"),
+            "url"        : "dashlet_servicestats.py",
+            "position"   : (31, 1),
+            "size"       : (30, 18),
+            "shadow"     : True,
+            "background" : True,
+            "refresh"    : 60,
+        },
+        {
+            "title"      : _("Host Problems (unhandled)"),
+            "title_url"  : "view.py?view_name=hostproblems&is_host_acknowledged=0",
+            "view"       : "hostproblems_notify_dash",
+            "position"   : (-1, 1),
+            "size"       : (GROW, 18),
+        },
+        {
+            "title"      : _("Service Problems (unhandled)"),
+            "title_url"  : "view.py?view_name=svcproblems&is_service_acknowledged=0",
+            "view"       : "svcproblems_notify_dash",
+            "position"   : (1, 19),
+            "size"       : (GROW, MAX),
+        },
+        {
+            "title"      : _("Events of recent 4 hours"),
+            "title_url"  : "view.py?view_name=events_dash",
+            "view"       : "events_dash",
+            "position"   : (-1, -1),
+            "size"       : (GROW, GROW),
+        },
+    ]
+}
