9ffc2341a19afc6c3916eedb84123c1b1a9d3248
Andreas Doehler <andreas.doehler@gmail.com>
2014-04-24 07:53:14 +0200

    Erster Hyper-V Check ob Zeitsync abgeschaltet ist auf den Maschinen (Sync zum Hyper-V Host

---
+++ checks/hyper-v/disabled-timesync/plugins/Disabletimesync.ps1
@@ -0,0 +1,4 @@
+﻿Write-Host -NoNewLine "<<<hyperv_disabletimesync>>>"
+
+$Server = hostname
+get-VMIntegrationService -name 'Zeitsynchronisierung' -Computername $Server -Vmname * | format-table -HideTableHeaders
