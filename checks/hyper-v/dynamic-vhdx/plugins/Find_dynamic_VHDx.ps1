c0e2843cd724070d834b503ab21eb372b330fc4a
Andreas Doehler <andreas.doehler@gmail.com>
2014-04-24 20:36:47 +0200

    Check für dynamische VHDX in einer Hyper-V Installation

---
+++ checks/hyper-v/dynamic-vhdx/plugins/Find_dynamic_VHDx.ps1
@@ -0,0 +1,20 @@
+﻿Write-Host -NoNewLine "<<<hyperv_dynamicvhdx:sep(59)>>>"
+Write-Host ""
+# Auslesen aller dynamischen Dateien
+$VMs = Get-SCVirtualMachine 
+
+foreach ($VM in $VMs)
+{
+    $Disks = Get-VM $VM | Get-SCVirtualHardDisk
+	foreach ($Disk in $Disks)
+	{
+		if ($Disk.VHDType -ne "FixedSize")
+		{
+			$DiskLocation = $Disk.Location
+			$CSVVolume = ($Disk.Location.Split("\"))[2]
+			$DiskName = ($Disk.Location.Split("\"))[-1]
+			$Value = $VM.Name + ";" + $DiskName + ";" + $CSVVolume + ";"
+            write-host $Value
+		}
+	}
+}
