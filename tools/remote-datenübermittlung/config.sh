98c2ef38cca40fdaa17e2810669b3c3706faa96e
Andreas Doehler <andreas.doehler@gmail.com>
2014-06-20 14:25:54 +0200

    kleine Anpassunge für einheitliche Schreibweise

--- tools/remote-datenübermittlung/config.sh
+++ tools/remote-datenübermittlung/config.sh
@@ -3,7 +3,6 @@
 cd ~/tmp/
 rm config_live
 ~/local/bin/livedump -C > ~/tmp/config_live
-echo password | gpg --batch -q --passphrase-fd 0 --cipher-algo AES256 -c ~/tmp/config_live
+echo <geheim> | gpg --batch -q --passphrase-fd 0 --cipher-algo AES256 -c ~/tmp/config_live
 mv ~/tmp/config_live.gpg ~/tmp/config_live.zip
 echo "config mail" | mutt -a ~/tmp/config_live.zip -s "config mail" nagios@bech-noc.de
-
--- tools/remote-datenübermittlung/import_data_to_nagios.sh
+++ tools/remote-datenübermittlung/import_data_to_nagios.sh
@@ -5,13 +5,13 @@ cd ~
 rm /usr/local/nagios/Maildir/new/*
 if [ -f /tmp/data_live.zip ];
 then
-    echo password | gpg --batch -q -o /usr/local/nagios/var/spool/checkresults/ca1b2c8 --passphrase-fd 0 --decrypt /tmp/data_live.zip
+    echo <geheim> | gpg --batch -q -o /usr/local/nagios/var/spool/checkresults/ca1b2c8 --passphrase-fd 0 --decrypt /tmp/data_live.zip
     touch /usr/local/nagios/var/spool/checkresults/ca1b2c8.ok
     rm /tmp/data_live.zip
 fi
 if [ -f /tmp/config_live.zip ];
 then
     rm /usr/local/nagios/etc/remote/skd.cfg
-    echo password | gpg --batch -q -o /usr/local/nagios/etc/remote/skd.cfg --passphrase-fd 0 --decrypt /tmp/config_live.zip
+    echo <geheim> | gpg --batch -q -o /usr/local/nagios/etc/remote/skd.cfg --passphrase-fd 0 --decrypt /tmp/config_live.zip
     rm /tmp/config_live.zip
 fi
