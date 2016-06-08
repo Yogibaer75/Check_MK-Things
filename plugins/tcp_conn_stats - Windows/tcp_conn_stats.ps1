15b64a416559a5b99f7c04dd3271e84e2fc32aeb
Andreas Doehler <andreas.doehler@gmail.com>
2016-06-08 15:52:01 +0200

    tcp_conn_stats Plugin fuer Windows als Powershell Script

---
+++ plugins/tcp_conn_stats - Windows/tcp_conn_stats.ps1
@@ -0,0 +1,13 @@
+﻿Write-Host "<<<tcp_conn_stats>>>"
+$stateList = "LISTEN", "TIME_WAIT", "ESTABLISHED", "FIN_WAIT2", "CLOSE_WAIT"
+$c = netstat -aonp TCP | % {$_.replace("ABH","LISTEN")} | % {$_.replace("WARTEND","TIME_WAIT")} | % {$_.replace("HERGESTELLT","ESTABLISHED")} | % {$_.replace("FIN_WARTEN_2","FIN_WAIT2")} | % {$_.replace("SCHLIESSEN_WARTEN","CLOSE_WAIT")}
+foreach($s in $stateList)
+{
+    $anzahl = 0
+    foreach($line in $c)
+    {   
+        $count = ([regex]::Matches($line, $s )).count
+        $anzahl = $anzahl + $count
+    }
+    Write-Host $s $anzahl
+}
