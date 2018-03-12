70eb47f9c5fb87905b0a8aa9ed9dbe3945b859bd
Andreas Döhler <andreas.doehler@gmail.com>
2018-03-12 15:02:42 +0100

    wrong timestamp corrected and put booth loops into one

--- plugins/fileinfo/fileinfo.ps1
+++ plugins/fileinfo/fileinfo.ps1
@@ -1,17 +1,22 @@
 Write-Output "<<<fileinfo:sep(124)>>>"
 $date1 = Get-Date -Date "01/01/1970"
+$filepath = "C:\Temp"
 $date_now = [int]([DateTime]::UtcNow - $date1).TotalSeconds
 Write-Output $date_now
-$files =  Get-ChildItem C:\Temp -rec | Where-Object {! $_.PSIsContainer} 
-foreach($object in $files)
-{
-    $time_1 = ($object.LastWriteTime).ToFileTime()
-    $timestamp = [int]([datetime]::fromfiletime($time_1) - $date1).TotalSeconds
-    Write-Host ($object.FullName,$object.length,$timestamp) -Separator "|"
-}
-$dirs = Get-ChildItem C:\Temp -rec | Where-Object {$_.PSIsContainer -eq $True} | Where-Object {$_.GetFiles().Count -eq 0}
+$dirs = Get-ChildItem $filepath -rec | Where-Object {$_.PSIsContainer -eq $True}
 foreach($object in $dirs)
 {
-    $missing_dir = $object.FullName + "\*.*"
-    Write-Host ($missing_dir,"missing",$date_now) -Separator "|"
+    if ($object.GetFiles().Count -eq 0) {
+        $missing_dir = $object.FullName + "\*.*"
+        Write-Host ($missing_dir,"missing",$date_now) -Separator "|"
+    }
+    else {
+        $files =  Get-ChildItem $object.FullName | Where-Object {! $_.PSIsContainer} 
+        foreach($object in $files)
+        {
+            $time_1 = ($object.LastWriteTime).ToFileTime()
+            $timestamp = [int]([datetime]::fromfiletimeutc($time_1) - $date1).TotalSeconds
+            Write-Host ($object.FullName,$object.length,$timestamp) -Separator "|"
+        }
+    }
 }
