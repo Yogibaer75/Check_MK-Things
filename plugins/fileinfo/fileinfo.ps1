dc7e578476852f675b4a5843537376a8cf32e0fe
Andreas Döhler <andreas.doehler@gmail.com>
2018-03-12 14:11:13 +0100

    extended fileinfo for missing information

--- plugins/fileinfo/fileinfo.ps1
+++ plugins/fileinfo/fileinfo.ps1
@@ -2,10 +2,16 @@ Write-Output "<<<fileinfo:sep(124)>>>"
 $date1 = Get-Date -Date "01/01/1970"
 $date_now = [int]([DateTime]::UtcNow - $date1).TotalSeconds
 Write-Output $date_now
-$files =  Get-ChildItem D:\Temp -rec | Where-Object {! $_.PSIsContainer} 
+$files =  Get-ChildItem C:\Temp -rec | Where-Object {! $_.PSIsContainer} 
 foreach($object in $files)
 {
     $time_1 = ($object.LastWriteTime).ToFileTime()
     $timestamp = [int]([datetime]::fromfiletime($time_1) - $date1).TotalSeconds
     Write-Host ($object.FullName,$object.length,$timestamp) -Separator "|"
+}
+$dirs = Get-ChildItem C:\Temp -rec | Where-Object {$_.PSIsContainer -eq $True} | Where-Object {$_.GetFiles().Count -eq 0}
+foreach($object in $dirs)
+{
+    $missing_dir = $object.FullName + "\*.*"
+    Write-Host ($missing_dir,"missing",$date_now) -Separator "|"
 }
