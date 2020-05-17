16b24fa91ab0e42c54cc2016f88f9efaf2859802
Andreas Döhler <andreas.doehler@gmail.com>
2020-05-17 19:23:08 +0200

    Plugin reformat and rework directory structure

---
+++ checks/tcp_conn_stats - Windows/agents/windows/plugins/tcp_conn_stats.ps1
@@ -0,0 +1,17 @@
+﻿Write-Host "<<<tcp_conn_stats>>>"
+$stateList = "LISTEN", "TIME_WAIT", "ESTABLISHED", "FIN_WAIT2", "CLOSE_WAIT"
+$c = netstat -aonp TCP | ForEach-Object {
+    $_ -replace "ABH", "LISTEN" `
+        -replace "WARTEND", "TIME_WAIT" `
+        -replace "HERGESTELLT", "ESTABLISHED" `
+        -replace "FIN_WARTEN_2", "FIN_WAIT2" `
+        -replace "SCHLIESSEN_WARTEN", "CLOSE_WAIT"
+}
+foreach ($s in $stateList) {
+    $anzahl = 0
+    foreach ($line in $c) {   
+        $count = ([regex]::Matches($line, $s )).count
+        $anzahl = $anzahl + $count
+    }
+    Write-Host $s $anzahl
+}
---
+++ checks/tcp_conn_stats - Windows/readme.md
@@ -0,0 +1,4 @@
+# tcp_conn_stats Windows
+
+The plugin is ported to Windows with Powershell.
+Output is the same as on Linux/Unix
