b94d86636931c52a0c30bb1c1472f5cf147e8ecc
Andreas Doehler <andreas.doehler@gmail.com>
2021-09-22 20:27:21 +0200

    Windows patch day plugin to show when was the last update installed

--- check plugins 2.0/windows_patch_day/agents/windows/plugins/windows_patch_day.ps1
+++ check plugins 2.0/windows_patch_day/agents/windows/plugins/windows_patch_day.ps1
@@ -1,15 +1,52 @@
 ###
 
-$pswindow = $host.ui.rawui
-$newsize = $pswindow.buffersize
-$newsize.height = 300
-$newsize.width = 200
-$pswindow.Set_BufferSize($newsize)
+$pshost = Get-Host              # Get the PowerShell Host.
+$pswindow = $pshost.UI.RawUI    # Get the PowerShell Host's UI.
+
+$newsize = $pswindow.BufferSize # Get the UI's current Buffer Size.
+$newsize.height = 300          # Set the new buffer's heigt to 300 lines.
+$newsize.width = 200            # Set the new buffer's width to 200 columns.
+$pswindow.buffersize = $newsize # Set the new Buffer Size as active.
+
+$newsize = $pswindow.windowsize # Get the UI's current Window Size.
+$newsize.width = 200            # Set the new Window Width to 200 columns.
+$pswindow.windowsize = $newsize # Set the new Window Size as active.
+
+[Console]::OutputEncoding = [System.Text.UTF8Encoding]::new($true)
 
 ###
 
-Write-Host("<<<windows_patch_day:sep(124)>>>")
-Get-CimInstance -ClassName win32_reliabilityRecords `
-| Where-Object {$_.sourcename -eq "Microsoft-Windows-WindowsUpdateClient"} `
-| Sort-Object timegenerated -desc `
-| ForEach-Object {write-host($_.productname + "|" + $_.timegenerated)}
+# config file directory
+$MK_CONFDIR = $env:MK_CONFDIR
+
+# Fallback if no MK_CONFDIR is set
+if (!$MK_CONFDIR) {
+    $MK_CONFDIR= "$env:ProgramData\checkmk\agent\config"
+}
+
+# Read the config file - attention this is no source of the file as it needs to be read in UTF-8
+$CONFIG_FILE="${MK_CONFDIR}\windows_patch_day_cfg.cfg"
+if (test-path -path "${CONFIG_FILE}" ) {
+    $values = Get-Content -Path "${CONFIG_FILE}" -Encoding UTF8 | Out-String | ConvertFrom-StringData
+    $filterstring = $values.filterstring.split("|")
+    $updatecount = $values.updatecount
+}
+
+# If no config file is loaded use the default settings
+if (!$updatecount) {
+    $updatecount = 30
+}
+
+if (!$filterstring) {
+    $filterstring = ''
+}
+[regex] $filter_regex ='(?i)^(' + (($filterstring |ForEach-Object {[regex]::escape($_)}) –join "|") + ')'
+
+Write-Host('<<<windows_patch_day:sep(124)>>>')
+$Searcher = (New-Object -ComObject Microsoft.Update.Session).CreateUpdateSearcher()
+$HistoryCount = $Searcher.GetTotalHistoryCount()
+$Searcher.QueryHistory(0, $HistoryCount) `
+| Where-Object { $_.title -notmatch $filter_regex -AND $_.title -ne $null } `
+| Sort-Object date -desc `
+| Select-Object -First $updatecount `
+| ForEach-Object { Write-Host($_.title + '|' + $_.date + '|' + $_.resultcode) }
