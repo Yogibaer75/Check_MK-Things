342942c7c977a1324652f2e4e958eb3520cc07b6
Andreas Doehler <andreas.doehler@gmail.com>
2022-02-20 22:15:23 +0100

    latest fix for PSUs reflected in source files

--- check plugins 2.0/hpe_ilo/web/plugins/wato/datasource_programs_ilo.py
+++ check plugins 2.0/hpe_ilo/web/plugins/wato/datasource_programs_ilo.py
@@ -1,5 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+# (c) Andreas Doehler <andreas.doehler@bechtle.com/andreas.doehler@gmail.com>
+
+# This is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# ails.  You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
 
 from cmk.gui.plugins.wato import (
     IndividualOrStoredPassword,
@@ -7,13 +20,13 @@ from cmk.gui.plugins.wato import (
     rulespec_registry,
 )
 from cmk.gui.plugins.wato.datasource_programs import (
-    RulespecGroupDatasourcePrograms,
-)
+    RulespecGroupDatasourcePrograms, )
 from cmk.gui.valuespec import (
     Dictionary,
     TextAscii,
     Transform,
 )
+
 
 def _valuespec_special_agents_ilo():
     return Transform(
@@ -24,13 +37,13 @@ def _valuespec_special_agents_ilo():
                     title=_("Username"),
                     allow_empty=False,
                 )),
-                ("password", IndividualOrStoredPassword(
-                    title=_("Password"),
-                    allow_empty=False,
-                )),
+                ("password",
+                 IndividualOrStoredPassword(
+                     title=_("Password"),
+                     allow_empty=False,
+                 )),
             ],
-        ),
-    )
+        ), )
 
 
 rulespec_registry.register(
@@ -40,4 +53,3 @@ rulespec_registry.register(
         title=lambda: _("Agent HPE iLO Configuration"),
         valuespec=_valuespec_special_agents_ilo,
     ))
-
