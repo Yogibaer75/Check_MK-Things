59b761774e468c6fc4083645a9244116daeb05e6
Andreas Doehler <andreas.doehler@gmail.com>
2022-10-19 21:12:08 +0200

    Windows dedup check

---
+++ check plugins 2.0/windows_dedup/agents/windows/plugins/windows_dedup.ps1
@@ -0,0 +1,7 @@
+Write-Host "<<<windows_dedup:sep(58)>>>"
+$VolumeList = Get-DedupVolume
+
+Foreach ($Vol in $VolumeList)
+{
+    $Vol | Format-List -Property Volume,Enabled,Capacity,FreeSpace,UsedSpace,UnoptimizedSize,SavedSpace,SavingsRate
+}
