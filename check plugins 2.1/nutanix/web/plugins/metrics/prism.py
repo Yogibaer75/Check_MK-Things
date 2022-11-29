91e1d0a4508a8bdf8bb3b826c9b969b6e5ab7da9
Andreas Doehler <andreas.doehler@gmail.com>
2022-11-29 22:23:01 +0100

    fixed df_translation for fs checks inside Nutanix

--- check plugins 2.1/nutanix/web/plugins/metrics/prism.py
+++ check plugins 2.1/nutanix/web/plugins/metrics/prism.py
@@ -3,3 +3,5 @@ from cmk.gui.plugins.metrics.utils import check_metrics
 from cmk.gui.plugins.metrics.translation import df_translation
 
 check_metrics["check_mk-prism_host_usage"] = df_translation
+check_metrics["check_mk-prism_containers"] = df_translation
+check_metrics["check_mk-prism_storage_pools"] = df_translation
