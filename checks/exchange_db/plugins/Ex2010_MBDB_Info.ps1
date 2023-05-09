b0c71a79c6be126b59a9be89b1e23b0a3770bcb9
Andreas Doehler <andreas.doehler@gmail.com>
2023-05-09 13:29:12 +0200

    reformat Exchange MBDB Info

--- checks/exchange_db/plugins/Ex2010_MBDB_Info.ps1
+++ checks/exchange_db/plugins/Ex2010_MBDB_Info.ps1
@@ -1,1 +1,5 @@
-Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010 ; Write-Host '<<<Ex2010_MBDB_Info>>>' ; $DBS = Get-MailboxDatabase -Status -Server (hostname) ; $DBS | % { '{0,-10} {1,-20} {2, -6}' -f ($_.name -replace ' ','_'), ($_.Databasesize -replace ',', ''-replace '^.*\((.*) bytes\)', '$1'), (Get-MailboxStatistics -Database $_.name).count };
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010
+Write-Host '<<<Ex2010_MBDB_Info>>>'
+Get-MailboxDatabase -Status -Server (hostname) | ForEach-Object { 
+    '{0,-10} {1,-20} {2, -6}' -f ($_.name -replace ' ', '_'), ($_.Databasesize -replace ',', '' -replace '^.*\((.*) bytes\)', '$1'), (Get-MailboxStatistics -Database $_.name).count 
+}
