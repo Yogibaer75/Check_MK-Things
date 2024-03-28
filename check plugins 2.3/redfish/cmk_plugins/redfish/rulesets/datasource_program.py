afa941e75039e3f87d8af201eeeb3b96ed21974f
Niko Wenselowski <niko.wenselowski@checkmk.com>
2024-03-28 11:51:00 +0100

    Fix name of range validator
    Based on fd1517f306d8ebe1a437c23605b7663e1acc749f

--- check plugins 2.3/redfish/cmk_plugins/redfish/rulesets/datasource_program.py
+++ check plugins 2.3/redfish/cmk_plugins/redfish/rulesets/datasource_program.py
@@ -18,9 +18,9 @@ from cmk.rulesets.v1.form_specs import (
     MultipleChoiceElement,
     Password,
     String,
-    validators,
 )
 from cmk.rulesets.v1.rule_specs import Topic, SpecialAgent
+from cmk.rulesets.v1.form_specs.validators import NumberInRange
 
 
 def _valuespec_special_agents_redfish() -> Dictionary:
@@ -128,7 +128,7 @@ def _valuespec_special_agents_redfish() -> Dictionary:
                         "Port number for connection to the Rest API. Usually 8443 (TLS)"
                     ),
                     prefill=DefaultValue(443),
-                    custom_validate=validators.InRange(min_value=1, max_value=65535),
+                    custom_validate=(NumberInRange(min_value=1, max_value=65535),),
                 ),
             ),
             "proto": DictElement(
@@ -160,7 +160,7 @@ def _valuespec_special_agents_redfish() -> Dictionary:
                         "Number of retry attempts made by the special agent."
                     ),
                     prefill=DefaultValue(10),
-                    custom_validate=validators.InRange(min_value=1, max_value=20),
+                    custom_validate=(NumberInRange(min_value=1, max_value=20),),
                 ),
             ),
             "timeout": DictElement(
@@ -170,7 +170,7 @@ def _valuespec_special_agents_redfish() -> Dictionary:
                         "Number of seconds for a single connection attempt before timeout occurs."
                     ),
                     prefill=DefaultValue(10),
-                    custom_validate=validators.InRange(min_value=1, max_value=20),
+                    custom_validate=(NumberInRange(min_value=1, max_value=20),),
                 ),
             ),
         },
