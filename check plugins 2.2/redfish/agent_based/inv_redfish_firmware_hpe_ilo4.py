87641d476c22fbcaa77d07827ceb2264b85fadbf
Andreas Doehler <andreas.doehler@gmail.com>
2024-07-31 16:27:25 +0200

    adjusting HW/SW inventory path for view and iLO4

--- check plugins 2.2/redfish/agent_based/inv_redfish_firmware_hpe_ilo4.py
+++ check plugins 2.2/redfish/agent_based/inv_redfish_firmware_hpe_ilo4.py
@@ -27,7 +27,7 @@ register.agent_section(
 
 def inventory_redfish_firmware_hpe_ilo4(section: RedfishAPIData) -> InventoryResult:
     """create inventory table for firmware"""
-    path = ["hardware", "firmware", "hpe"]
+    path = ["hardware", "firmware", "redfish"]
     padding = len(str(len(section)))
     for index, entry_id in enumerate(section):
         entry = section.get(entry_id)
